2a493be387e536b5c76bb224642a3758abc6a722
zhishi <1951671751@qq.com>
2025-07-05 17:14:08 +0800

    no message

--- src/pages/lottery/components/map.vue
+++ src/pages/lottery/components/map.vue
@@ -1,14 +1,9 @@
 <template>
-	<view class='mapBox fc'>
-	</view>
+  <view class="mapBox fc"></view>
 </template>
 
 <script setup>
-	import {
-		onMounted,
-		ref
-	} from 'vue';
+import { onMounted, ref } from "vue";
 </script>
 
-<style lang='scss' scoped>
-</style>
+<style lang="scss" scoped></style>
